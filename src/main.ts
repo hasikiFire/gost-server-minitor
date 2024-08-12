270639aba80e7a82d07f2edead59d508ba411321
hasikiFire <gedune@outlook.com>
2024-08-12 23:28:47 +0800

    feat: bootstrap

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,16 @@ import { GostService } from './module/gost/gost.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app); // 配置 Swagger
   if (!process.env.NODE_ENV?.includes('development')) {
     const dockerCommand = new DockerCommand();
     // 1. 重启 Docker
     dockerCommand.restartDocker();
     const configurationService = app.get(GostService);
-    await configurationService.loadConfig();
     // 2. 以最新的数据库数据加载配置
-    // 3.
+    await configurationService.loadConfig();
   }
 
-  setupSwagger(app); // 配置 Swagger
   await app.listen(9000);
 }
 bootstrap();
