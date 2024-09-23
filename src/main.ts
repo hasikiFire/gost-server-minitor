f88c667b07922aa694f4e326800dcfcba7cac430
hasikiFire <gedune@outlook.com>
2024-09-23 23:16:07 +0800

    feat: docker ccompose

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DockerCommand } from './command/docker';
 import { setupSwagger } from 'swagger.config';
 import { ConfigureService } from './module/configure/configure.service';
 import { ReqeustInterceptor } from './common/requestInterceptor';
@@ -12,11 +11,6 @@ async function bootstrap() {
   const logger = app.get(MyLoggerService);
   app.useGlobalInterceptors(new ReqeustInterceptor());
   setupSwagger(app); // 配置 Swagger
-  if (!process.env.NODE_ENV?.includes('development')) {
-    const dockerCommand = new DockerCommand();
-    // 1. 重启 Docker
-    dockerCommand.restartDocker();
-  }
   // 2. 以最新的数据库数据加载配置
   const configureServiceService = app.get(ConfigureService);
   await configureServiceService.loadConfig();
