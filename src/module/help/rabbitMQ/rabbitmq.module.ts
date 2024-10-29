06a46d8872f54c54b67dae29d3fbb44049732b42
hasikiFire <gedune@outlook.com>
2024-10-30 00:44:07 +0800

    fix: redis

--- src/module/help/rabbitMQ/rabbitmq.module.ts
+++ src/module/help/rabbitMQ/rabbitmq.module.ts
@@ -4,7 +4,7 @@ https://docs.nestjs.com/modules
 
 import { Global, Module } from '@nestjs/common';
 import { RabbitMQService } from './rabbitmq.service';
-import { GatewayModule } from '../gateway/gateway.module';
+import { GatewayModule } from 'src/module/gateway/gateway.module';
 
 @Global()
 @Module({
