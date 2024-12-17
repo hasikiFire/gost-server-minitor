9707f6ad15565c41553d3553241ed33892a1190a
hasikiFire <gedune@outlook.com>
2024-12-17 15:52:39 +0800

    fix: remove main load

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from 'src/config/swagger/swagger.config';
-import { ConfigureService } from 'src/module/configure/configure.service';
 import { ReqeustInterceptor } from 'src/common/interceptor/requestInterceptor';
 
 async function bootstrap() {
@@ -10,8 +9,6 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ReqeustInterceptor());
   setupSwagger(app); // 配置 Swagger
   // 2. 以最新的数据库数据加载配置
-  const configureService = app.get(ConfigureService);
-  await configureService.loadConfig();
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
